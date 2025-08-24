1651688641c01dd60223dd2ee0aea4aca13d7a87
shaikh24 <89303966+shaikh24@users.noreply.github.com>
2025-08-24 14:59:59 +0500

    Update mango.js

--- server/src/db/mango.js
+++ server/src/db/mango.js
@@ -1,1 +1,17 @@
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+dotenv.config({ path: '.envx' });
 
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri);
+
+export async function connectMongo() {
+  try {
+    await client.connect();
+    console.log("✅ MongoDB Connected!");
+    return client;
+  } catch (err) {
+    console.error("❌ MongoDB Connection Error:", err);
+    process.exit(1);
+  }
+}
