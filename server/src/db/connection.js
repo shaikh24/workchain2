72324e4486b7709aa00cee58ea274438eb87d79f
shaikh24 <89303966+shaikh24@users.noreply.github.com>
2025-08-24 14:14:02 +0500

    Update connection.js

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -1,1 +1,18 @@
+const { MongoClient } = require('mongodb');
 
+const uri = process.env.MONGO_URI; // password safe way se env file me
+
+const client = new MongoClient(uri);
+
+async function connectDB() {
+  try {
+    await client.connect();
+    console.log("MongoDB Atlas Connected!");
+    return client;
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
